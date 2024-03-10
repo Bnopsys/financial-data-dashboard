310f8a66ab011c7da7f84cc64fd2cd9cacc3c23b
Bnopsys <roddystones@yahoo.com>
2024-03-10 14:24:02 -0700

    making class to have better structure. Also added some things that need to be added to class as well as function which can be imported by mainfile to not work with class at all.

--- codefiles/std_data.py
+++ codefiles/std_data.py
@@ -8,8 +8,36 @@ File is to take some of the data burden off analytical file.
 import pandas as pd
 
 class StandardDeviationData:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.data = df
+        self.mean = self.data.describe()['mean']
+        self.std = self.data.describe()['std']
+        self.twenty_five_percent = self.data.describe()['25%']
+        self.seventy_five_percent = self.data.describe()['75%']
+
+    def categorical_describe(self, category):
+        ...
+        # the get_df_describe function has to make a table. Id rather just do the calculations
+        # myself.
+
+    # first step: get described data and dataframe filtered based on category
+        # replace described data for function based on getting 25% and 75% data
+    # second step: use described data to return outliers 
+
+    # third step: Make an empty list(or dict: if its a dict it could have row_no: row_data) which can hold the outlier rows
+
+    # fourth step: loop over rows in dataframe(im using items which may not be the best approach maybe i should use iterrows or something else)
+
+    # fith step: for each row in dataframe check if it is an outlier
+
+    # sixth step: if not, skip. If so, take the row and add it to the list/dict.
+
+    # seventh step: remove the outlier line from the main dataframe
+
+    # eighth step: create a dataframe from the list/dict values.
+        
+    # TODO use the standard deviation and mean metrics to give the user an idea on where the data falls and how to better understand the data.
+
 
 
 
@@ -66,6 +94,22 @@ def identifying_outliers_df_before_concat(df:pd.DataFrame, outliers_df: pd.DataF
     
 
 if __name__ == '__main__':
-    ...
+    file = '/Users/roddystones/Documents/datafiles/main_datafile.csv'
+    df1 = pd.read_csv(file)
+
+    testcls = StandardDeviationData(df1)
+
+    # testing describe function first
+    print(df1[df1['Category'] == 'Restaurants'])
+    print(get_df_describe(df1, 'Restaurants'))
     # test class here
     
+
+"""
+# This is a facade design pattern approach which hides the complexity of the class inside of the function.
+# can be used after creating the class.
+
+def run_standard_deviation(data: pd.DataFrame, categories):
+    sdd = StandardDeviationData(data)
+    return sdd.run(categories)
+"""
