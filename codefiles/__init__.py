d6b8fdbabdaffe152cb1eb36f8345e997a92870a
Bnopsys <roddystones@yahoo.com>
2024-03-20 05:53:21 -0700

    added std_data to import

--- codefiles/__init__.py
+++ codefiles/__init__.py
@@ -1,9 +1,10 @@
 from .main_df_module import Maindf
 from .main_df_creation import MainfileCreation
+from .std_data import StandardDeviationData
 from .analytical_data import *
 from .graphs import boxplot
 
 __all__ = ['total_income', 'total_expenses', 'find_top_five_purchases',
            'data_stats', 'identifying_payments', 'tracking_payments', 
            'categorical_describe', 'budget_deviation', 'boxplot', 'Maindf', 
-           'MainfileCreation']
+           'MainfileCreation', 'StandardDeviationData']
